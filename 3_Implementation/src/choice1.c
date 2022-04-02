7d427023152cf18cfbf033d8738b87fe986d4eb8
Avanish Maurya <67328173+avnish8726@users.noreply.github.com>
2022-04-02 21:20:20 +0530

    final commit

---
+++ 3_Implementation/src/choice1.c
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+#include "function.h"
+int enterchoice(void)
+{
+	int choice;
+	printf("                 Welcome To Movie Ticket Booking System\n");
+	printf(" ************#######************\n");
+	printf("             1- Admin           \n");
+	printf("             2- User          \n");
+	printf("             3- Exit system:                                   \n");
+	printf("**********************\n");
+	printf("  Enter your choice: ");
+	scanf("%d",&choice);
+	return choice;
+=======
+#include "function.h"
+int enterchoice(void)
+{
+	int choice;
+	printf("                 Welcome To Movie Ticket Booking System\n");
+	printf(" ************#######************\n");
+	printf("             1- Admin           \n");
+	printf("             2- User          \n");
+	printf("             3- Exit system:                                   \n");
+	printf("**********************\n");
+	printf("  Enter your choice: ");
+	scanf("%d",&choice);
+	return choice;
+>>>>>>> 076561460ebab2c9670a951f1f986fa6f8384e74
+}
---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,52 @@
+<<<<<<< HEAD
+#include "unity.h"
+#include "function.h"
+
+void setUp()
+{
+
+}
+void tearDown()
+{
+  
+}
+void test_changepriceticket(void)
+{
+    int price1=700;
+    TEST_ASSERT_EQUAL(700,changepriceticket(price1));
+    
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  
+  RUN_TEST(test_changepriceticket);
+  return UNITY_END();
+=======
+#include "unity.h"
+#include "function.h"
+
+void setUp()
+{
+
+}
+void tearDown()
+{
+  
+}
+void test_changepriceticket(void)
+{
+    int price1=700;
+    TEST_ASSERT_EQUAL(700,changepriceticket(price1));
+    
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  
+  RUN_TEST(test_changepriceticket);
+  return UNITY_END();
+>>>>>>> 076561460ebab2c9670a951f1f986fa6f8384e74
+}
